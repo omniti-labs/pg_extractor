3adbc5b5b980135fd15543f7dfb86ebf85e74f1c
Keith Fiske <keith@keithf4.com>
2016-08-31 12:19:26 -0400

    Version bump to 2.3.4

--- pg_extractor.py
+++ pg_extractor.py
@@ -22,7 +22,7 @@ class PGExtractor:
     """
 
     def __init__(self):
-        self.version = "2.3.3"
+        self.version = "2.3.4"
         self.args = False
         self.temp_filelist = []
         self.error_list = []
